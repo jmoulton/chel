2fbcee55cfd93a97bd92cb8c95b6bc38986a1267
Justin Moulton <moultonjust@gmail.com>
2017-07-12 17:21:26 -0400

    Now supports Eddie

--- app/controllers/chel_controller.rb
+++ app/controllers/chel_controller.rb
@@ -1,41 +1,5 @@
 class ChelController < ApplicationController
 
-  EMRICK_QUOTES = %W(
-    What\ magic\ to\ even\ get\ the\ shot\ away
-    Gaining\ space...maybe\ time.
-    Drilled\ into\ the\ turnbuckle
-    A\ lightening\ like\ glove
-    Got\ that\ in\ the\ trapper!
-    The\ draw,\ the\ tie-up,\ and\ his\ team\ prevails
-    Two\ guys\ together\ with\ force...BIG\ force
-    The\ stick\ can\ break\ up\ a\ play...and\ it\ just\ DID!
-    Forcing\ attention\ in\ deep
-    WAFFLEBOARDED!
-    He\ got\ REEFED!
-    A\ HAMMER\ SHOT!
-    That\ shot\ HIT\ A\ MAN!
-    What\ a\ RIPPER!
-    Big\ sweeping\ save!
-    Will\ this\ be\ icing?\ Aaaand\ it\ will\ be!\ on\ the\ defense\ arriving\ first
-    oh\ he’ll\ pay\ for\ that\ one!
-    My\ goodness!
-    No\ team\ worth\ its\ name\ will\ stand\ for\ this
-    THAT\ WAS\ DEFENSE!!
-    Moving\ to\ the\ shadow\ of\ the\ boards
-    Gets\ in\ what'll\ he\ do?
-    Two\ sticks\ together\ they\ cancel
-    Big\ hit...possesion\ GONE!
-    HIT\ THE\ POST\ WITH\ THE\ SHOT!!!
-    What\ a\ skill\ level\ there!
-    Goalie\ to\ the\ bench\ extra\ attacker\ out
-    NET\ OPEN!
-    Solid\ win\ at\ center\ ice,\ now\ what?
-    He\ ran\ right\ over\ the\ goalie!
-    One\ of\ the\ best\ parts\ about\ this\ game\ is\ two\ big\ men\ colliding
-    Oh\ did\ ever\ get\ mashed!
-    ...and\ it\ is\ played\ by\ the\ largest\ stick\ on\ the\ ice
-  )
-
   def new_match
     player = ":#{params[:user_name]}:"
     match = Match.create(players: [player], max_players: 4)
@@ -46,7 +10,11 @@ class ChelController < ApplicationController
   end
 
   def random_quote
-    ":docemrick: '#{EMRICK_QUOTES.sample(1).first}'"
+    if rand > 0.333
+      ":docemrick: '#{EMRICK_QUOTES.sample(1).first}'"
+    else
+      ":eddie: '#{EDDIE_QUOTES.sample(1).first}'"
+    end
   end
 
   def button
---
+++ config/initializers/quotes.rb
@@ -0,0 +1,45 @@
+EMRICK_QUOTES = %W(
+  What\ magic\ to\ even\ get\ the\ shot\ away
+  Gaining\ space...maybe\ time.
+  Drilled\ into\ the\ turnbuckle
+  A\ lightening\ like\ glove
+  Got\ that\ in\ the\ trapper!
+  The\ draw,\ the\ tie-up,\ and\ his\ team\ prevails
+  Two\ guys\ together\ with\ force...BIG\ force
+  The\ stick\ can\ break\ up\ a\ play...and\ it\ just\ DID!
+  Forcing\ attention\ in\ deep
+  WAFFLEBOARDED!
+  He\ got\ REEFED!
+  A\ HAMMER\ SHOT!
+  That\ shot\ HIT\ A\ MAN!
+  What\ a\ RIPPER!
+  Big\ sweeping\ save!
+  Will\ this\ be\ icing?\ Aaaand\ it\ will\ be!\ on\ the\ defense\ arriving\ first
+  oh\ he’ll\ pay\ for\ that\ one!
+  My\ goodness!
+  No\ team\ worth\ its\ name\ will\ stand\ for\ this
+  THAT\ WAS\ DEFENSE!!
+  Moving\ to\ the\ shadow\ of\ the\ boards
+  Gets\ in\ what'll\ he\ do?
+  Two\ sticks\ together\ they\ cancel
+  Big\ hit...possesion\ GONE!
+  HIT\ THE\ POST\ WITH\ THE\ SHOT!!!
+  What\ a\ skill\ level\ there!
+  Goalie\ to\ the\ bench\ extra\ attacker\ out
+  NET\ OPEN!
+  Solid\ win\ at\ center\ ice,\ now\ what?
+  He\ ran\ right\ over\ the\ goalie!
+  One\ of\ the\ best\ parts\ about\ this\ game\ is\ two\ big\ men\ colliding
+  Oh\ did\ ever\ get\ mashed!
+  ...and\ it\ is\ played\ by\ the\ largest\ stick\ on\ the\ ice
+  SHOOOOOTING!!!
+  That's\ a\ good\ solid\ hit,\ he'll\ feel\ that\ one\
+)
+
+EDDIE_QUOTES = %w(
+  Oh\ that\ shot\ just\ hit\ his\ glove
+  He\ stick\ handled\ himself\ into\ submission!
+  I\ thought\ the\ second\ save\ was\ better
+  You\ can't\ go\ around\ and\ hit\ everybody\ ESPECIALLY\ if\ they\ don't\ have\ the\ puck
+  The\ goalie\ makes\ himself\ look\ big
+)
